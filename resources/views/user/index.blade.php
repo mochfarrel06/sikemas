c16a3cae01e37f947d5c91f722174d66da6870d3
fremasadi <fremasadii@gmail.com>
2025-07-03 19:15:15 +0700

    feat

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -85,7 +85,7 @@
         
         <div class="row justify-content-center">
             <div class="col-12 col-xl-10">
-                <div class="container">
+                <div class="mobile-jkn-container">
                     <div class="download-section">
                         <div class="download-buttons">
                             <a href="#" class="download-btn">
